1018743b5cca6e28f85645b694f742eb735742a8
Riccardo Marini <riccardo.marini65@gmail.com>
2020-12-06 21:03:35 +0100

    wip

--- TheWorld_ClientDll.h
+++ TheWorld_ClientDll.h
@@ -7,7 +7,12 @@
 #pragma warning(disable : 4049)
 
 #include "targetver.h"
+
+#ifdef WIN32_LEAN_AND_MEAN
+#else
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#endif
+
 // Windows Header Files:
 #include <windows.h>
 
